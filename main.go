9469845bbf4a7c394a9f4f8c3ef5d48827f31b49
jaemjeon <piepie1346@gmail.com>
2025-02-07 16:34:28 +0900

    make coin web, add work with POST method

--- main.go
+++ main.go
@@ -15,15 +15,35 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/pages/home.html"))
 	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
-const port string = ":4000"
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", "Add Page")
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockdata")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+
+}
+
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.html"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.html"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("i'm listening http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
