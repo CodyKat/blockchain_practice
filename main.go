3593fccf67a49f04a1f8b9e61e317ee693e5d7f7
jaemjeon <piepie1346@gmail.com>
2025-02-07 23:52:57 +0900

    add POST method to add block

--- main.go
+++ main.go
@@ -5,22 +5,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/CodyKat/blockchain_practice/blockchain"
+	"github.com/CodyKat/blockchain_practice/utils"
 )
 
 const port string = ":4000"
 
+type URL string
+type AddBlockBody struct {
+	Message string
+}
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL         string
-	Method      string
-	Description string
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+func (u URLDescription) String() string {
+	return "I'm URL Description"
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Description",
+		},
+		{
+			URL:         URL("/blocks"),
+			Method:      "POST",
+			Description: "Add a block",
+			Payload:     "data:string",
 		},
 	}
 	rw.Header().Add("Content-type", "Application/json")
@@ -31,8 +55,22 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(rw, "%s", b)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-type", "Application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockChain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockChain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
