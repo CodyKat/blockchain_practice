03ad4cf3f6fbbc27164e3e3d44cf940873e29965
jaemjeon <piepie1346@gmail.com>
2025-02-12 01:22:56 +0900

    make cli to select html | rest, port

---
+++ cli/cli.go
@@ -0,0 +1,33 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/CodyKat/blockchain_practice/explorer"
+	"github.com/CodyKat/blockchain_practice/rest"
+)
+
+func usage() {
+	fmt.Println("hello my name is jaemjeon")
+	fmt.Println("Please use the following flags :")
+	fmt.Println("-port: 	Set the PORT of the server")
+	fmt.Println("=mode: 	Start the REST API")
+}
+
+func Start() {
+	port := flag.Int("port", 4000, "Sets port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+	fmt.Println(*port, *mode)
+}
--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Println("hello my name is jaemjeon")
-	fmt.Println("Please use the following command :")
-	fmt.Println("explorer : Start the HTML explorer")
-	fmt.Println("rest : 	Start the REST API")
-}
+import "github.com/CodyKat/blockchain_practice/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-		os.Exit(0)
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of this server")
-
-	switch os.Args[1] {
-	case "explorer":
-	case "rest":
-		rest.Parse(os.Args[2:])
-		fmt.Println(*portFlag)
-	default:
-		usage()
-	}
+	cli.Start()
 }
